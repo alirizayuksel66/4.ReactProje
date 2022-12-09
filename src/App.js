f3c97c2b0913a2bc99031384409caeac6c803518
Ali Rıza Yüksel <114735512+alirizayuksel66@users.noreply.github.com>
2022-12-09 16:28:40 +0300

    Form Elemanları
    Form Elamanları Kullanıldı

--- src/App.js
+++ src/App.js
@@ -17,6 +17,29 @@ function App() {
     {key : 3, value: 'Ayşegül'},
     {key : 4, value: 'Cevat'},
   ]
+  const selectedCategories = categories && categorylist.filter(c => categories.includes(c.key))
+  const [rule, setRule] = useState(true)
+  const [rules, setRules] = useState([
+    {key: 1 , value: '1. Kuralı Kabul Ediyorum', checked: false},
+    {key: 2 , value: '2. Kuralı Kabul Ediyorum', checked: false},
+    {key: 3 , value: '3. Kuralı Kabul Ediyorum', checked: true},
+  ])
+  const checkRule = (key, checked) => {
+    setRules(rules => rules.map(rule => {
+      if (key == rule.key) {
+        rule.checked = checked
+      }
+      return rule
+    }))
+  }
+  const enabled = rules.every(rule => rule.checked)
+  const [level,setLevel] = useState('jr_developer')
+  const levels = [
+    {key: 'beginner', value: 'Baslangıç'},
+    {key: 'jr_developer', value: 'Jr. Developer'},
+    {key: 'sr_developer', value: 'Sr. Developer'},
+  ]
+  const selectedLevel = levels.find(g => g.key == level)
 
   return(
     <>
@@ -39,7 +62,32 @@ function App() {
         ))}
       </select>
       <br></br>
-      <pre>{JSON.stringify(categories, null, 2)}</pre>
+      <pre>{JSON.stringify(selectedCategories, null, 2)}</pre>
+      <br></br>
+      <label>
+          <input type="checkbox" checked={rule} onChange={e => setRule(e.target.checked)}></input>
+          Kuralları Kabul Ediyorum
+      </label>
+      <br></br>
+      {rules.map(rule => (
+        <label key={rule.key}>
+          <input type="checkbox" checked={rule.checked} onChange={e => checkRule(rule.key, e.target.checked)}></input>
+          {rule.value}
+        </label>
+      ))}
+      <br></br>
+      <pre>{JSON.stringify(rules, null, 2)}</pre>
+      <br></br>
+      {levels.map((levell, index) => (
+        <label key={index}>
+          <input type="radio" value={levell.key} checked={levell.key == level} onChange={e => setLevel(e.target.value)}></input>
+          {levell.value}
+        </label>
+      ))}
+      <br></br>
+      {JSON.stringify(selectedLevel)}
+      <br></br>
+      <button disabled={!enabled}>Devam Et</button>
     </>
   );
 }
